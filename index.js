17204921843ed3e68eba10edf7d590df36b6c840
Ankit Raj Srivastava <ankitrajsrivastava222@gmail.com>
2023-01-17 21:36:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+function show1(){
+    document.getElementById('single1').style.height="100px"
+    document.getElementById('single1').style.display="block"
+    document.getElementById('single2').style.display="none"
+    document.getElementById('single3').style.display="none"
+    document.getElementById('single4').style.display="none"
+    document.getElementById('single5').style.display="none"
+    document.getElementById('show1').style.display='none'
+
+}
+function show2(){
+    document.getElementById('single2').style.height="100px"
+    document.getElementById('single2').style.display="block"
+    document.getElementById('single1').style.display="none"
+    document.getElementById('single3').style.display="none"
+    document.getElementById('single4').style.display="none"
+    document.getElementById('single5').style.display="none"
+    document.getElementById('show2').style.display='none'
+
+}
+function show3(){
+    document.getElementById('single3').style.height="100px"
+    document.getElementById('single3').style.display="block"
+    document.getElementById('single1').style.display="none"
+    document.getElementById('single2').style.display="none"
+    document.getElementById('single4').style.display="none"
+    document.getElementById('single5').style.display="none"
+    document.getElementById('show3').style.display='none'
+
+}
+function show4(){
+    document.getElementById('single4').style.height="100px"
+    document.getElementById('single4').style.display="block"
+    document.getElementById('single1').style.display="none"
+    document.getElementById('single2').style.display="none"
+    document.getElementById('single3').style.display="none"
+
+    document.getElementById('single5').style.display="none"
+    document.getElementById('show4').style.display='none'
+
+}
+function show5(){
+    document.getElementById('single5').style.height="100px"
+    document.getElementById('single5').style.display="block"
+    document.getElementById('single1').style.display="none"
+    document.getElementById('single2').style.display="none"
+    document.getElementById('single3').style.display="none"
+    document.getElementById('single4').style.display="none"
+    document.getElementById('show5').style.display='none'
+
+}
